2eca0f72817d6ca7ac26d2ce24da66deac0bc316
tsboudot <tsboudot@gmail.com>
2023-07-13 15:17:06 +0200

    Début travail ApptPage.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar__logo"><img src='/logo.png' alt="logo" /></div>
-            <p>Acceuil</p>
-            <p>A propos</p>
+            <Link to="/">Acceuil</Link>
+            <Link to="/about">A propos</Link>
         </nav>
     );
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './styles/index.css';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import ApptPage from './Components/ApptPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +12,9 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/flat" element={<ApptPage />} />
+        <Route path="/about" element={<h1>Ceci est un about</h1>} />
+        <Route path="*" element={<h1>404 Not found</h1>} />
       </Routes>
     </Router>
   </React.StrictMode>
