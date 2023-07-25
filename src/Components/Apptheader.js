05e4d990f42b71c1d4987d6b115897412ccac98f
tsboudot <tsboudot@gmail.com>
2023-07-25 17:13:30 +0200

    Style apptPage: headerOk, infoContainer +ou-

--- src/Components/Apptheader.js
+++ src/Components/Apptheader.js
@@ -4,10 +4,18 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 function Apptheader({ appt }) {
-    const stars = [];
-    for (let i = 0; i < appt.rating; i++) {
-        stars.push(<span key={i}>&#9733;</span>) // &#9733 est le code HTML pour l'étoile
-    }  // Recevez l'appartement actuel comme prop
+    const activeStars = [];
+    const inactiveStars = [];
+    const rating = appt.rating;
+
+    for (let i = 0; i < rating; i++) {
+        activeStars.push(<span key={i} className="activeStar">&#9733;</span>);
+    }
+
+    for (let i = rating; i < 5; i++) {
+        inactiveStars.push(<span key={i} className="inactiveStar">&#9733;</span>);
+    }
+
     return (
         <div className="apptHeader">
             <Carousel>
@@ -17,27 +25,33 @@ function Apptheader({ appt }) {
                     </div>
                 ))}
             </Carousel>
-            <div className="apptHeaderTop">
+            <div className="apptHeaderInfos">
+                <div className="apptHeaderLeft">
+                    <div className="apptHeaderTittle&loc">
+                        <h3 className="apptTittle">{appt.title}</h3> {/* utilisez la prop pour afficher le titre */}
+                        <p className="apptLocation">{appt.location}</p>
+                    </div>
+                    <div className="apptHeadertags">
+
+                        <ul>{appt.tags.map((tag, index) => (
+                            <li className="tagAppart" key={index}>{tag}</li>
+                        ))}
+                        </ul>
+                    </div>
+                </div>
+
                 <div className="apptHeaderRight">
-                    <h1 className="apptTittle">{appt.title}</h1> {/* utilisez la prop pour afficher le titre */}
-                    <p className="apptLocation">{appt.location}</p>
-                </div>
-                <div className="appHeaderLeft__host">
-                    <div className="apptHostName"></div>
-                    <div className="appptHostPicture"></div>
-                </div>
-            </div>
-            <div className="apptHeaderBottom">
-                <div className="ratings">
-                    {stars}
-                </div>
-                <div className="tags">
-                    <ul>{appt.tags.map((tag, index) => (
-                        <li className="tagAppart" key={index}>{tag}</li>
-                    ))}
-                    </ul>
+                    <div className="apptHeaderHost">
+                        <div className="apptHostName"><h4>{appt.host.name}</h4></div>
+                        <div className="apptHostPicture"><img src={appt.host.picture} alt={`Host: ${appt.host.name}`} className="apptHostImg" /></div>
+                    </div>
+                    <div className="ratings">
+                        {activeStars}
+                        {inactiveStars}
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 };
--- src/Components/Apptinfos.js
+++ src/Components/Apptinfos.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons';
 
 function Apptinfos({ appt }) {
-    console.log(appt.description);
-    console.log(appt.equipement);
+    const [showDescription, setShowDescription] = useState(false);
+    const [showEquipment, setShowEquipment] = useState(false);
+
     return (
         <div className="apptInfos">
             <div className="description">
-                <h2>Description</h2>
-                <p className="fetchDescription">{appt.description}</p>
+                <div className="apptInfosHeader">
+                    <h2>Description</h2>
+                    <button onClick={() => setShowDescription(!showDescription)} className={showDescription ? "rotated" : ""}>
+                        <FontAwesomeIcon icon={faCaretUp} />
+                    </button>
+
+                </div>
+                <div className={`apptInfosContent ${showDescription ? "visible" : ""}`}>
+                    {showDescription && <p className="fetchDescription">{appt.description}</p>}
+                </div>
             </div>
             <div className="equipement">
-                <h2>Equipements</h2>
-                <ul className="fetchEquipement">
-                    {appt.equipments.map((equipment, index) => (
-                        <li key={index} className="equip">{equipment}</li>
-                    ))}
-                </ul>
+                <div className="apptInfosHeader"><h2>Equipements </h2>
+                    <button onClick={() => setShowEquipment(!showEquipment)} className={showEquipment ? "rotated" : ""}>
+                        <FontAwesomeIcon icon={faCaretUp} />
+                    </button>
+
+                </div>
+                <div className={`apptInfosContent ${showEquipment ? "visible" : ""}`}>
+                    {showEquipment &&
+                        <ul className="fetchEquipement">
+                            {appt.equipments.map((equipment, index) => (
+                                <li key={index} className="equip">{equipment}</li>
+                            ))}
+                        </ul>
+                    }
+                </div>
             </div>
         </div>
     );
